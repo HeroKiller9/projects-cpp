8d3eaf4dee3476aa80b5b3be57778ef35edf1925
HeroKiller9 <mohammed532170@gmail.com>
2025-12-01 22:49:33 +0300

    تحديث البروجكت

--- main.cpp
+++ main.cpp
@@ -2,11 +2,36 @@
 
 int main()
     {
-  std::cout << "nyaaaaa";
+  
 
-  return 0;
+      std::cout << "************************\n";
+      std::cout << "Name: Hero killer9\n";
+      std::cout << "Age: 19 Years\n";
+      std::cout << "City: Al-hasa\n";
+      std::cout << "Country: saudi Arabia\n";
+      std::cout << "***********************" << std::endl << std::endl;
 
+      std::cout << "************\n";
+      std::cout << "************\n";
+      std::cout << "************\n";
+      std::cout << "************" << std::endl << std::endl;
 
+      std::cout << "I love programming!" << std::endl << std::endl;
+      std::cout << "I promise to be the best developer ever!" << std::endl << std::endl;
+      std::cout << "I know it will take some time to practice, but I will achieve my goal" << std::endl << std::endl;
+      std::cout << "best Regards\n";
+      std::cout << "Hero Killer9" << std::endl << std::endl;
+      
+
+      std::cout << "*     " "*\n";
+      std::cout << "*     " "*\n";
+      std::cout << "*******\n";
+      std::cout << "*     " "*\n";
+      std::cout << "*     " "*\n";
+      
+      
+
+      
   }
 
 
