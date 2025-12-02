e2cee04fb0155f13912ebd51747efcc74e3a6b8f
HeroKiller9 <mohammed532170@gmail.com>
2025-12-02 21:18:30 +0300

    Add namespace

--- main.cpp
+++ main.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
+using namespace std; 
+  
+
 
 int main()
     {
   
 
-      std::cout << "************************\n";
-      std::cout << "Name: Hero killer9\n";
-      std::cout << "Age: 19 Years\n";
-      std::cout << "City: Al-hasa\n";
-      std::cout << "Country: saudi Arabia\n";
-      std::cout << "***********************" << std::endl << std::endl;
+      cout << "************************\n";
+      cout << "Name: Hero killer9\n";
+      cout << "Age: 19 Years\n";
+      cout << "City: Al-hasa\n";
+      cout << "Country: saudi Arabia\n";
+      cout << "***********************" << endl << endl;
 
-      std::cout << "************\n";
-      std::cout << "************\n";
-      std::cout << "************\n";
-      std::cout << "************" << std::endl << std::endl;
+      cout << "************\n";
+      cout << "************\n";
+      cout << "************\n";
+      cout << "************" << endl << endl;
 
-      std::cout << "I love programming!" << std::endl << std::endl;
-      std::cout << "I promise to be the best developer ever!" << std::endl << std::endl;
-      std::cout << "I know it will take some time to practice, but I will achieve my goal" << std::endl << std::endl;
-      std::cout << "best Regards\n";
-      std::cout << "Hero Killer9" << std::endl << std::endl;
+      cout << "I love programming!" << endl << endl;
+      cout << "I promise to be the best developer ever!" << endl << std::endl;
+      cout << "I know it will take some time to practice, but I will achieve my goal" << endl << endl;
+      cout << "best Regards\n";
+      cout << "Hero Killer9" << endl << endl;
       
 
-      std::cout << "*     " "*\n";
-      std::cout << "*     " "*\n";
-      std::cout << "*******\n";
-      std::cout << "*     " "*\n";
-      std::cout << "*     " "*\n";
+      cout << "*     " "*\n";
+      cout << "*     " "*\n";
+      cout << "*******\n";
+      cout << "*     " "*\n";
+      cout << "*     " "*\n";
       
-      
+     return 0;
 
       
   }
