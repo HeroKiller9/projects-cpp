2cfa55e80bdef18cb6aad69cc881c88fc1418748
HeroKiller9 <mohammed532170@gmail.com>
2026-01-22 16:40:53 +0300

    Statement c++

--- Conditional_Statements.cpp
+++ Conditional_Statements.cpp
@@ -23,8 +23,41 @@ int main() {
 
   cout << "meow" << endl;
 
+  /*   Switch .. Case       */
 
+  int day = 1;
 
+  cout << "enter your Num day?" << endl;
+    cin >> day;
+
+  switch (day) {
+
+  
+    case 1:
+      cout << "Sun" << endl;
+      break;
+    case 2:
+      cout << "Monday" << endl;
+      break;
+    case 3:
+      cout << "Tuesday" << endl;
+      break;
+    case 4:
+      cout << "Wednesday" << endl;
+      break;
+    case 5:
+    cout << "Thursday" << endl;
+    break;
+    case 6:
+    break;
+    cout << "Friday" << endl;
+    break;
+    case 7:
+    cout << "Saturday" << endl;
+    break;
+   default:
+    cout << "Not a week day!" << endl;
+  }
 
 
 
